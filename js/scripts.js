c35b326c03c5df2cb8e912751a857e6fd1777c05
eduardsnovosads <eduardixx007@gmail.com>
2021-04-20 16:52:01 +0300

    nepabeigts darbs

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,26 @@
+const POP_UP = document.getElementById('popUp');
+let biblioteka = [];
+
 document.getElementById('pievienotPieIepirkumuSaraksta').addEventListener('click', () => {
-    console.log("Poga nospiesta!");
-
-    let popUp = document.getElementById('popUp');
-
-    popUp.style.display = 'block'
+    POP_UP.style.display = 'block';
 })
+
+document.getElementById('pievienotListam').addEventListener('click', () => {
+    POP_UP.style.display = 'none';
+
+    let produkts = {nosaukums: nosaukums.value, daudzums: daudzums.value};
+
+    nosaukums.value = "";
+    daudzums.value = "";
+
+    biblioteka.push(produkts);
+
+    console.log(biblioteka);
+
+})
+
+
+function render() {
+    let plaukts = document.getElementById('plaukts');
+    
+}
