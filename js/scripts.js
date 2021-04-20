3c871f26397cad88b907e323dd319fc42020ade1
eduardsnovosads <eduardixx007@gmail.com>
2021-04-20 16:08:42 +0300

    Pievienot gramatu modelis

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,7 @@
-console.log("JavaScrip works")
+document.getElementById('pievienotPieIepirkumuSaraksta').addEventListener('click', () => {
+    console.log("Poga nospiesta!");
+
+    let popUp = document.getElementById('popUp');
+
+    popUp.style.display = 'block'
+})
